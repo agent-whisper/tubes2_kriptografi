0ef83cb1e69b9f953ae8643feba20e5fc0ec2b37
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-04-09 07:35:29 +0700

    Added interface for sha module

---
+++ src/cryptography/sha.py
@@ -0,0 +1,10 @@
+def sign(message, private_key):
+    """ Returns message appended with digital sign
+    example: 'message'<div id="digital_sign"><p>sign</p></div>
+    """
+    pass
+
+def verify(message, sign, public_key):
+    """ Returns True if sign matches
+    """
+    pass
